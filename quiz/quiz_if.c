899c366c6c9abb78f93c7674be14bd2d76617944
AD58-3104 <s19C1012SK@s.chibakoudai.jp>
2020-10-16 01:11:08 +0900

    add quiz if --- function

---
+++ quiz/quiz_if.c
@@ -0,0 +1,18 @@
+/*
+if文に関する問題です。sumの値を出力(printfする)するように13行目のifの条件を書いてください。
+*/
+
+
+
+#include <stdio.h>
+int main()
+{
+    int x = 0, y = 20;
+    int sum = 0;
+    sum = x + y;
+    if (/*    　ここに記入する          */)
+    {
+        printf("%d", sum);
+    }
+    return 0;
+}
